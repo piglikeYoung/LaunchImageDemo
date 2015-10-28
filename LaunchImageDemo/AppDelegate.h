6f530b6227f16056ef59acec518716b1cdc1573f
piglikeyoung <316046510@qq.com>
2015-10-28 09:33:14 +0800

    Initial Commit

---
+++ LaunchImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LaunchImageDemo
+//
+//  Created by piglikeyoung on 15/10/28.
+//  Copyright © 2015年 pikeYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
